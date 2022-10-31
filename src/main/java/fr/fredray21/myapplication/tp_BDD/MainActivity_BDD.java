79e21102293c0034282b9b8f483cdbb88db7151b
Frédéric Dabadie <dabadie.frederic@gmail.com>
2022-10-31 15:51:50 +0100

    edit MediaPlayer && add TP5 - BDD

---
+++ src/main/java/fr/fredray21/myapplication/tp_BDD/MainActivity_BDD.java
@@ -0,0 +1,38 @@
+package fr.fredray21.myapplication.tp_BDD;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import fr.fredray21.myapplication.R;
+
+public class MainActivity_BDD extends AppCompatActivity {
+
+    helperBDD bdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bdd);
+        bdd = new helperBDD(this);
+        EditText pays = findViewById(R.id.id_input_pays);
+        EditText devise = findViewById(R.id.id_input_devise);
+        TextView contentBDD = findViewById(R.id.id_txtView_contentBDD);
+        Button btnAdd = findViewById(R.id.id_btn_stockBDD);
+
+        btnAdd.setOnClickListener((v) -> {
+                    bdd.insertPays(pays.getText().toString(), devise.getText().toString());
+                    contentBDD.setText(String.join("\n", bdd.getAllPays()));
+                }
+        );
+
+        Button btnClearBDD = findViewById(R.id.id_btn_clearBDD);
+        btnClearBDD.setOnClickListener((v) -> {
+            bdd.clearBDD();
+            contentBDD.setText(String.join("\n", bdd.getAllPays()));
+        });
+    }
+}
---
+++ src/main/java/fr/fredray21/myapplication/tp_BDD/helperBDD.java
@@ -0,0 +1,66 @@
+package fr.fredray21.myapplication.tp_BDD;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class helperBDD extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "BDD.db";
+    public static final String TABLE_NAME = "repertoire";
+    public static final String COL_ID = "ID";
+    public static final String COL_PAYS = "PAYS";
+    public static final String COL_DEVISE = "DEVISE";
+
+    public helperBDD(Context ctx) {
+        super(ctx, "BDD", null, 1);
+    }
+
+    @Override
+    public void onCreate(android.database.sqlite.SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_PAYS + " TEXT, " + COL_DEVISE + " TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(android.database.sqlite.SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertPays(String pays, String devise) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_PAYS, pays);
+        contentValues.put(COL_DEVISE, devise);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public ArrayList<String> getAllPays() {
+        ArrayList<String> res = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor q = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        q.moveToFirst();
+        while (!q.isAfterLast()) {
+            @SuppressLint("Range") String id = q.getString(q.getColumnIndex(COL_ID));
+            @SuppressLint("Range") String nom = q.getString(q.getColumnIndex(COL_PAYS));
+            @SuppressLint("Range") String tel = q.getString(q.getColumnIndex(COL_DEVISE));
+            res.add(id + " - " + nom + " - " + tel);
+            q.moveToNext();
+        }
+        return res;
+    }
+
+    public void clearBDD() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_NAME);
+    }
+}
